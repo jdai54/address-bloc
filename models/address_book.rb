0d58f78a42426e7a5c05c306ab7aa2ceb3c0d7a0
Jake <jdai54@gmail.com>
2016-01-13 15:19:33 -0800

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,4 +18,14 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def remove_entry(name, phone_number, email)
+    @entries.each_with_index do |value, index|
+      if value.name == name && value.phone_number == phone_number && value.email == email
+        @entries.delete_at(index)
+        break
+      end
+    end
+
+  end
+
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,32 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    before :each do
+      @book = AddressBook.new
+      @book.add_entry('Jake Dai', '12345678', 'jake@dai.com')
+      @book.add_entry('Joe Smith', '9876543210', 'joesmith@hello.com')
+    end
+    it 'does not delete entry if it does not exist' do
+      expect(@book.entries.size).to eq(2)
+      @book.remove_entry('Joe S', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(@book.entries.size).to eq(2)
+    end
+    it 'deletes only the passed entry' do
+      expect(@book.entries.size).to eq(2)
+      @book.remove_entry('Jake Dai', '12345678', 'jake@dai.com')
+      expect(@book.entries.size).to eq(1)
+    end
+    it 'deletes correct entry' do
+      expect(@book.entries.size).to eq(2)
+      @book.remove_entry('Jake Dai', '12345678', 'jake@dai.com')
+      @book.entries.each do |entry|
+        expect(entry.name).not_to eq 'Jake Dai'
+        expect(entry.phone_number).not_to eq '12345678'
+        expect(entry.email).not_to eq 'jake@dai.com'
+      end
+
+    end
+  end
+
 end
