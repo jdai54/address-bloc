da3cf5edec44922299763f4002c52bbaff517878
Jake <jdai54@gmail.com>
2016-01-22 18:37:11 -0800

    Checkpoint 21 reading CSVs

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -3,7 +3,7 @@ require_relative '../models/address_book'
 RSpec.describe AddressBook do
   let(:book) { AddressBook.new }
 
-def check_entry(entry, expect_name, expect_number, expected_email)
+def check_entry(entry, expected_name, expected_number, expected_email)
   expect(entry.name).to eq expected_name
   expect(entry.phone_number).to eq expected_number
   expect(entry.email).to eq expected_email
@@ -58,45 +58,33 @@ end
       # Check the first entry
       entry_one = book.entries[0]
       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-
     end
 
-      it "imports the 2nd entry" do
+    it "imports the 2nd entry" do
       book.import_from_csv("entries.csv")
       # Check the second entry
-      entry_one = book.entries[1]
+      entry_two = book.entries[1]
       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
-
     end
-
       it "imports the 3rd entry" do
       book.import_from_csv("entries.csv")
       # Check the third entry
-      entry_one = book.entries[2]
+      entry_three = book.entries[2]
       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
-
-    
     end
 
-      it "imports the 4th entry" do
+    it "imports the 4th entry" do
       book.import_from_csv("entries.csv")
       # Check the fourth entry
-      entry_one = book.entries[3]
-
-      expect(entry_one.name).to eq "Sally"
-      expect(entry_one.phone_number).to eq "555-555-4646"
-      expect(entry_one.email).to eq "sally@blocmail.com"
+      entry_four = book.entries[3]
+      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
     end
 
-      it "imports the 5th entry" do
+    it "imports the 5th entry" do
       book.import_from_csv("entries.csv")
       # Check the fifth entry
-      entry_one = book.entries[4]
-
-      expect(entry_one.name).to eq "Sussie"
-      expect(entry_one.phone_number).to eq "555-555-2036"
-      expect(entry_one.email).to eq "sussie@blocmail.com"
-    end
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 
     end
 
