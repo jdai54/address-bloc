a6a6390a415e6bc85feb8123974cf1d9612a76c7
Jake <jdai54@gmail.com>
2016-01-23 21:55:17 -0800

    Assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -88,6 +88,37 @@ end
 
     end
 
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      # Check the size of the entires in AddressBook
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the first entry
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the second entry
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the third entry
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Sherry", "1231231234", "sherry@sherry.com")
+    end
+
   end
+
+end
 
 end
